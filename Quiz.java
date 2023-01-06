2d91fb96d2b49f61b210e1d5e92b0f347994add3
Donita Rose Brazil <116091553+44iqh@users.noreply.github.com>
2023-01-07 02:35:33 +0800

    Create Quiz.java

---
+++ Quiz.java
@@ -0,0 +1,12 @@
+Class Student {
+      private String name;
+ 
+      public Student(String name)  {
+            this.name = name;
+      }
+
+       public String getName() {
+           return name;
+
+     }
+   }
